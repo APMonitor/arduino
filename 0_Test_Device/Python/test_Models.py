3625a789da8b17562c9bd5ca31f770c0495faec1
APMonitor <john@apmonitor.com>
2020-05-07 14:37:54 -0600

    Add files via upload

--- 0_Test_Device/Python/test_Models.py
+++ 0_Test_Device/Python/test_Models.py
@@ -2,12 +2,38 @@ import tclab
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+# FOPDT model
+Kp = 0.5      # degC/%
+tauP = 120.0  # seconds
+thetaP = 10   # seconds (integer)
+Tss = 23      # degC (ambient temperature)
+Qss = 0       # % heater
+
+# define energy balance model
+def heat(x,t,Q):
+    # Parameters
+    Ta = 23 + 273.15   # K
+    U = 10.0           # W/m^2-K
+    m = 4.0/1000.0     # kg
+    Cp = 0.5 * 1000.0  # J/kg-K    
+    A = 12.0 / 100.0**2 # Area in m^2
+    alpha = 0.01       # W / % heater
+    eps = 0.9          # Emissivity
+    sigma = 5.67e-8    # Stefan-Boltzman
+    
+    # Temperature State 
+    T = x[0]
+    
+    # Nonlinear Energy Balance
+    dTdt = (1.0/(m*Cp))*(U*A*(Ta-T) \
+            + eps * sigma * A * (Ta**4 - T**4) \
+            + alpha*Q)
+    return dTdt
 
 # Connect to Arduino
 a = tclab.TCLab()
-
-# Get Version
-print(a.version)
 
 # Turn LED on
 print('LED On')
@@ -27,10 +53,18 @@ T1 = np.ones(loops) * a.T1 # measured T (degC)
 Tsp2 = np.ones(loops) * 23.0 # set point (degC)
 T2 = np.ones(loops) * a.T2 # measured T (degC)
 
-# step test (0 - 100%)
+# Predictions
+Tp = np.ones(loops) * a.T1
+error_eb = np.zeros(loops)
+Tpl = np.ones(loops) * a.T1
+error_fopdt = np.zeros(loops)
+
+# impulse tests (0 - 100%)
 Q1 = np.ones(loops) * 0.0
 Q2 = np.ones(loops) * 0.0
-Q1[10:] = 80.0
+Q1[10:110] = 50.0 # step up for 100 sec
+Q1[200:300] = 90.0 # step up for 100 sec
+Q1[400:500] = 70.0 # step up for 100 sec
 
 print('Running Main Loop. Ctrl-C to end.')
 print('  Time   Q1     Q2    T1     T2')
@@ -54,7 +88,7 @@ try:
         sleep_max = 1.0
         sleep = sleep_max - (time.time() - prev_time)
         if sleep>=0.01:
-            time.sleep(sleep)
+            time.sleep(sleep-0.01)
         else:
             time.sleep(0.01)
 
@@ -68,9 +102,17 @@ try:
         T1[i] = a.T1
         T2[i] = a.T2
 
-        ###############################
-        ### CONTROLLER or ESTIMATOR ###
-        ###############################
+        # Simulate one time step with Energy Balance
+        Tnext = odeint(heat,Tp[i-1]+273.15,[0,dt],args=(Q1[i-1],))
+        Tp[i] = Tnext[1]-273.15
+        error_eb[i] = error_eb[i-1] + abs(Tp[i]-T1[i])
+
+        # Simulate one time step with FOPDT model
+        z = np.exp(-dt/tauP)
+        Tpl[i] = (Tpl[i-1]-Tss) * z \
+                 + (Q1[max(0,i-int(thetaP)-1)]-Qss)*(1-z)*Kp \
+                 + Tss
+        error_fopdt[i] = error_fopdt[i-1] + abs(Tpl[i]-T1[i])
 
         # Write output (0-100)
         a.Q1(Q1[i])
@@ -85,13 +127,21 @@ try:
 
         # Plot
         plt.clf()
-        ax=plt.subplot(2,1,1)
+        ax=plt.subplot(3,1,1)
         ax.grid()
-        plt.plot(tm[0:i],T1[0:i],'ro',label=r'$T_1$')
-        plt.plot(tm[0:i],T2[0:i],'bx',label=r'$T_2$')
+        plt.plot(tm[0:i],T1[0:i],'ro',label=r'$T_1$ measured')
+        plt.plot(tm[0:i],Tp[0:i],'k-',label=r'$T_1$ energy balance')
+        plt.plot(tm[0:i],Tpl[0:i],'g:',label=r'$T_1$ FOPDT')
+        plt.plot(tm[0:i],T2[0:i],'bx',label=r'$T_2$ measured')
         plt.ylabel('Temperature (degC)')
+        plt.legend(loc=2)
+        ax=plt.subplot(3,1,2)
+        ax.grid()
+        plt.plot(tm[0:i],error_eb[0:i],'k-',label='Energy Balance')
+        plt.plot(tm[0:i],error_fopdt[0:i],'g:',label='Linear')
+        plt.ylabel('Cumulative Error')
         plt.legend(loc='best')
-        ax=plt.subplot(2,1,2)
+        ax=plt.subplot(3,1,3)
         ax.grid()
         plt.plot(tm[0:i],Q1[0:i],'r-',label=r'$Q_1$')
         plt.plot(tm[0:i],Q2[0:i],'b:',label=r'$Q_2$')
@@ -107,8 +157,8 @@ try:
     # Save text file
     a.save_txt(tm[0:i],Q1[0:i],Q2[0:i],T1[0:i],T2[0:i],Tsp1[0:i],Tsp2[0:i])
     # Save figure
-    plt.savefig('test_Heaters.png')
-        
+    plt.savefig('test_Models.png')
+    
 # Allow user to end loop with Ctrl-C           
 except KeyboardInterrupt:
     # Disconnect from Arduino
@@ -117,7 +167,7 @@ except KeyboardInterrupt:
     print('Shutting down')
     a.close()
     a.save_txt(tm[0:i],Q1[0:i],Q2[0:i],T1[0:i],T2[0:i],Tsp1[0:i],Tsp2[0:i])
-    plt.savefig('test_Heaters.png')
+    plt.savefig('test_Models.png')
     
 # Make sure serial connection still closes when there's an error
 except:           
@@ -127,5 +177,5 @@ except:
     print('Error: Shutting down')
     a.close()
     a.save_txt(tm[0:i],Q1[0:i],Q2[0:i],T1[0:i],T2[0:i],Tsp1[0:i],Tsp2[0:i])
-    plt.savefig('test_Heaters.png')
+    plt.savefig('test_Models.png')
     raise
