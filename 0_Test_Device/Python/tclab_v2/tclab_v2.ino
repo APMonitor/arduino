3d362d16ec9c51761aa3c6b0b9072386ea7f5955
APMonitor <john@apmonitor.com>
2020-06-01 17:40:27 -0600

    Binary output support and additional controls

--- 0_Test_Device/Python/tclab_v2/tclab_v2.ino
+++ 0_Test_Device/Python/tclab_v2/tclab_v2.ino
@@ -1,7 +1,7 @@
 /*
   TCLab Temperature Control Lab Firmware
-  Jeffrey Kantor, Bill Tubbs, John Hedengren
-  May 2019
+  Jeffrey Kantor, Bill Tubbs, John Hedengren, Shawn Summey
+  June 2020
 
   This firmware provides a high level interface to the Temperature Control Lab. The
   firmware scans the serial port for commands. Commands are case-insensitive. Any
@@ -13,11 +13,15 @@
   P2 float  set pwm limit on heater 2, range 0 to 255. Default 100. Returns P2.
   Q1 float  set Heater 1, range 0 to 100. Returns value of Q1.
   Q2 float  set Heater 2, range 0 to 100. Returns value of Q2.
+  Q1B float set Heater 1, range 0 to 100. Returns value of Q1 as a 32-bit float.
+  Q2B float set Heater 2, range 0 to 100. Returns value of Q2 as a 32-bit float.
   R1        get value of Heater 1, range 0 to 100
   R2        get value of Heater 2, range 0 to 100
   SCAN      get values T1 T2 Q1 Q1 in line delimited values
   T1        get Temperature T1. Returns value of T1 in °C.
   T2        get Temperature T2. Returns value of T2 in °C.
+  T1B       get Temperature T1. Returns value of T1 in °C as a 32-bit float.
+  T2B       get Temperature T2. Returns value of T2 in °C as a 32-bit float.
   VER       get firmware version string
   X         stop, enter sleep mode. Returns "Stop"
 
@@ -59,6 +63,10 @@
       1.4.3 required Arduino IDE Version >= 1.0.0
       1.5.0 remove webusb
       1.6.0 average 10 temperature measurements to reduce noise
+      2.0.0 added binary communications.
+            T1B and T2B commands return 32-bit float
+            Q1B and Q2B commands return 32-bit float confirmation of heater setting
+            added calculation to use 1.75 AREF to match TMP36 voltage range 
 */
 
 #include "Arduino.h"
@@ -78,7 +86,7 @@
 const bool DEBUG = false;
 
 // constants
-const String vers = "1.4.3";   // version of this firmware
+const String vers = "3.0.0";   // version of this firmware
 const long baud = 115200;      // serial baud rate
 const char sp = ' ';           // command separator
 const char nl = '\n';          // command terminator
@@ -146,10 +154,11 @@ inline float readTemperature(int pin) {
   float degC = 0.;
   for (int i = 0; i < n; i++) {
     // (analogRead * 3300.0/1024.0 - 500.0)/10.0
-    degC += analogRead(pin) * 0.322265625 - 50.0;
+    degC += analogRead(pin) * 0.322265625 - 50.0;    // use for 3.3v AREF
+    //degC += analogRead(pin) * 0.170898438 - 50.0;  // use for 1.75v AREF
   }
   // return average from n reads
-  return degC / n;
+  return degC / float(n);
 }
 
 void parseCommand(void) {
@@ -178,6 +187,16 @@ void sendResponse(String msg) {
   Serial.println(msg);
 }
 
+void sendFloatResponse(float val) {
+  Serial.println(String(val,3));
+}
+
+void sendBinaryResponse(float val)
+{
+  byte *b = (byte*)&val;
+  Serial.write(b,4);  
+}
+
 void dispatchCommand(void) {
   if (cmd == "A") {
     setHeater1(0);
@@ -199,29 +218,43 @@ void dispatchCommand(void) {
   }
   else if (cmd == "Q1") {
     setHeater1(val);
-    sendResponse(String(Q1));
+    sendFloatResponse(Q1);
+  }
+  else if (cmd == "Q1B") {
+    setHeater1(val);
+    sendBinaryResponse(Q1);
   }
   else if (cmd == "Q2") {
     setHeater2(val);
-    sendResponse(String(Q2));
+    sendFloatResponse(Q2);
+  }
+  else if (cmd == "Q2B") {
+    setHeater1(val);
+    sendBinaryResponse(Q2);
   }
   else if (cmd == "R1") {
-    sendResponse(String(Q1));
+    sendFloatResponse(Q1);
   }
   else if (cmd == "R2") {
-    sendResponse(String(Q2));
+    sendFloatResponse(Q2);
   }
   else if (cmd == "SCAN") {
-    sendResponse(String(readTemperature(pinT1)));
-    sendResponse(String(readTemperature(pinT2)));
-    sendResponse(String(Q1));
-    sendResponse(String(Q2));
+    sendFloatResponse(readTemperature(pinT1));
+    sendFloatResponse(readTemperature(pinT2));
+    sendFloatResponse(Q1);
+    sendFloatResponse(Q2);
   }
   else if (cmd == "T1") {
-    sendResponse(String(readTemperature(pinT1)));
+    sendFloatResponse(readTemperature(pinT1));
+  }
+  else if (cmd == "T1B") {
+    sendBinaryResponse(readTemperature(pinT1));
   }
   else if (cmd == "T2") {
-    sendResponse(String(readTemperature(pinT2)));
+    sendFloatResponse(readTemperature(pinT2));
+  }
+  else if (cmd == "T2B") {
+    sendBinaryResponse(readTemperature(pinT2));
   }
   else if (cmd == "VER") {
     sendResponse("TCLab Firmware " + vers + " " + boardType);
